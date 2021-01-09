4097413f8fb5a6ad3610644409762c57e6e750db
Ansh Goyal <54253002+Ansh063@users.noreply.github.com>
2021-01-09 20:29:16 +0530

    Update chess_engine.js

--- chess_engine.js
+++ chess_engine.js
@@ -5,7 +5,53 @@ var newGame = new Chess()
 function onStart(source, piece, position, staus) {
     if(newGame.game_over() == true){
         return false;
-    }
+    }var board = null
+var newGame = new Chess()
+
+function onDragStart (source, piece, position, orientation) {
+  if (newGame.game_over())
+  {
+    return false
+  } 
+
+  if (piece.search(/^b/) !== -1)
+  { 
+    return false
+  }
+}
+
+function makeRandomMove () {
+  var possibleMoves = newGame.moves()
+
+  if (possibleMoves.length === 0) return
+
+  var randomIndex = Math.floor(Math.random() * possibleMoves.length)
+  newGame.move(possibleMoves[randomIndex])
+  board.position(newGame.fen())
+}
+
+function onDrop (source, target) {
+  var move = newGame.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  })
+  if (move === null) return 'snapback'
+  window.setTimeout(makeRandomMove, 250)
+}
+
+function onSnapEnd () {
+  board.position(newGame.fen())
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
     if(piece.search(/^b/) !== -1){
         return false;
     }
